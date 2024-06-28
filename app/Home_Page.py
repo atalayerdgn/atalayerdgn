4b06a2eccf96cc9ac39bdfba7b82c685a79accb3
atalayerdgn <atalayerdgnn@gmail.com>
2024-06-29 02:40:45 +0300

    push

---
+++ app/Home_Page.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import os
+from streamlit.components.v1 import components
+
+def main():
+    st.set_page_config(
+        page_title="Home Page",
+        page_icon=":computer:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+    st.title("Welcome to Homepage :handshake:")
+    st.write("Written by : [Atalay Erdogan] :brain:")
+    st.markdown("---")
+    st.write("Here is my social media: :smiling_face_with_smiling_eyes_and_hand_covering_mouth:")
+    st.markdown("<style>h6{color: darkblue;}</style>", unsafe_allow_html=True)
+    st.write("LinkedIn: <https://www.linkedin.com/in/atalay-erdoğan-39aba8285>", unsafe_allow_html=True)
+    st.write("Github: <https://github.com/atalayerdgn>", unsafe_allow_html=True)
+    st.write("Medium:<https://medium.com/@atalayerdgnn>", unsafe_allow_html=True)
+    st.markdown("---")
+
+    with st.form("Pages"):
+        Pages = st.radio('Pages:', ['Data Analysis & Processing', 'CheatSheets', 'My Projects', 'About Me','Time Series','Image Analysis and Processing'])
+        st.markdown("---")
+        submit_button = st.form_submit_button(label='Go to Page')
+        if submit_button:
+            if Pages == 'Data Analysis & Processing':
+                st.switch_page('pages/DataAnalysis&Processing.py')
+            if Pages == 'CheatSheets':
+                st.switch_page('pages/CheatSheets.py')
+            if Pages == 'My Projects':
+                st.switch_page('pages/Projects.py')
+            if Pages == 'About Me':
+                st.switch_page('pages/AboutMe.py')
+            if Pages == 'Time Series':
+                st.switch_page('pages/TimeSeriesWithPython.py')
+            if Pages == 'Image Analysis and Processing':
+                st.switch_page('pages/ImageAnalysis&Processing.py')
+            st.markdown("---")
+if __name__ == "__main__":
+    main()
---
+++ app/pages/CheatSheets.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+import streamlit_pdf_viewer as stf
+def main() -> None:
+    """
+    This function is used to show the Cheat Sheets.
+    """
+    st.set_page_config(
+        page_title="Web Application for Data Science",
+        page_icon=":computer:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+    st.title("Cheat Sheets")
+    st.markdown("---")
+    with st.form("cheatsheets"):
+        cheatsheets = st.radio('Select a cheat sheet:', ['SQL','Maths For ML','Pandas','Numpy','Linear Regression','Logistic Regression','Regression Problems'])
+        st.markdown("---")
+        submit_button = st.form_submit_button(label='Show Cheat Sheet')
+        if submit_button and cheatsheets == 'SQL':
+            st.switch_page('pages/SQL_Cheat_Sheet.py')
+        elif submit_button and cheatsheets == 'Maths For ML':
+            st.switch_page('pages/MathsForMachineLearning.py')
+if __name__ == "__main__":
+    main()
