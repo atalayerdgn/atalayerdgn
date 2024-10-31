8bb77416b819a5ba3be0889958714dc18a04a524
USER <USER@MacBook-Air-3.local>
2024-10-31 12:29:08 +0300

    push

--- app/pages/AboutMe.py
+++ app/pages/AboutMe.py
@@ -23,14 +23,8 @@ class AboutMe:
             with st.expander("Türkçe"):
                 st.write("Merhaba. Lisans düzeyi eğitim öğretim hayatıma Gebze Teknik Üniversitesinde İktisat bölümü okuyarak başladım. Verilen eğitimin yetersiz olduğunu düşünerek okulumu bıraktım. \
                         Sonra 42 Kocaeli ile tanıştım ve kendimi istediğim alanda geliştirme fırsatı buldum. Bu okulda algoritma bazlı C ve C++ eğitimi görüyorum. Açıkcası benim için verimli bir tecrübe oluyor.\
-                        Kendimi bu konularda geliştirdikten sonra kendimi keşfetmeye karar verdim ve bir anda kendimi Veri Bilimi alanında buldum. Yakın zamanda bu alanda projeler geliştirdim ve geliştirmeye devam ediyorum.")
-        with col2:
-            with st.expander("English"):
-                st.write("Hello. I started my undergraduate education life by studying Economics at Gebze Technical University. \
-                        I left my school thinking that the education given was insufficient. Then I met 42 Kocaeli and had the opportunity to develop myself in the field I wanted. \
-                        In this school, I study algorithm-based C and C +++. Frankly, it is a productive experience for me. \
-                        After developing myself in these subjects, I decided to discover myself and suddenly found myself in the field of Data Science. \
-                        I have recently developed projects in this field and I continue to develop them.")
+                        Kendimi bu konularda geliştirdikten sonra kendimi keşfetmeye karar verdim ve bir anda kendimi Veri Bilimi alanında buldum. Yakın zamanda bu alanda projeler geliş ediyorum. Şu anda Sakarya Uygulamalı Bilimler Üniversitesi \
+                        Büyük Veri Analistliği Bölümünde okuyorum.")
         st.subheader("Skills")
         col3, col4 = st.columns(2)
         with col3:
